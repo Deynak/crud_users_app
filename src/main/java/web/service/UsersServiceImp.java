3f0959f63b6345d2b3cf45c58306e1fc556c1997
Deynak <miron-ox@mail.ru>
2024-08-16 06:12:01 +0300

    final commit

--- src/main/java/web/service/UsersServiceImp.java
+++ src/main/java/web/service/UsersServiceImp.java
@@ -1,6 +1,5 @@
 package web.service;
 
-
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import web.dao.UsersDao;
@@ -9,7 +8,6 @@ import web.entity.Users;
 import java.util.List;
 
 @Component
-@Transactional
 public class UsersServiceImp implements UsersService {
 
     private final UsersDao usersDao;
@@ -18,28 +16,32 @@ public class UsersServiceImp implements UsersService {
         this.usersDao = usersDao;
     }
 
-
     @Override
+    @Transactional(readOnly = true)
     public List<Users> readingAllUsers() {
         return usersDao.readingAllUsers();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Users readUser(Long id) {
         return usersDao.readUser(id);
     }
 
     @Override
+    @Transactional
     public void saveUser(Users user) {
         usersDao.saveUser(user);
     }
 
     @Override
+    @Transactional
     public void updateUser(Long id, Users user) {
         usersDao.updateUser(id, user);
     }
 
     @Override
+    @Transactional
     public void deleteUser(Long id) {
         usersDao.deleteUser(id);
     }
